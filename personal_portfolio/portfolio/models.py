d3d7cfe1d5e0706767ef17a7fce6fd0a867b18b8
Fedorov Anton <afedorov43@gmail.com>
2022-04-19 19:59:32 +0300

    Config admin panel, reworked project class.

--- personal_portfolio/portfolio/models.py
+++ personal_portfolio/portfolio/models.py
@@ -2,7 +2,14 @@ from django.db import models
 
 
 class Project(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.CharField(max_length=255)
-    image = models.ImageField(upload_to='portfolio/images/')
-    url = models.URLField(blank=True)
+    title = models.CharField(verbose_name='Заголовок', max_length=100)
+    description = models.CharField(verbose_name='Описание', max_length=255)
+    image = models.ImageField(verbose_name='Изображение', upload_to='portfolio/images/')
+    url = models.URLField(verbose_name='Ссылка', blank=True)
+
+    class Meta():
+        verbose_name = 'Проект'
+        verbose_name_plural = 'Проекты'
+
+    def __str__(self):
+        return self.title
