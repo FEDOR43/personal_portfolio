7c05a49e6fb52bfc2a2fce8f452acae7c7cde16f
Fedorov Anton <afedorov43@gmail.com>
2022-09-11 20:40:56 +0300

    Added blog

--- personal_portfolio/blog/models.py
+++ personal_portfolio/blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(verbose_name='Заголовок', max_length=200)
+    published = models.DateTimeField(verbose_name='Дата и время публикации', auto_now_add=True)
+    blog_text = models.TextField(verbose_name='Текст', max_length=1000)
+
+    class Meta():
+        verbose_name = 'Блог'
+        verbose_name_plural = 'Блоги'
+
+    def __str__(self):
+        return f'{self.title}, опубликован {self.published}'
