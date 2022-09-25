85b3911afc5af369abd5dde667930b9e0af197f8
Fedorov Anton <afedorov43@gmail.com>
2022-09-25 14:22:15 +0300

    Config admin panel.

--- personal_portfolio/portfolio/admin.py
+++ personal_portfolio/portfolio/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Project
 
-admin.site.register(Project)
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    empty_value_display = 'unknown'
+    list_display = ('title', 'description', 'image', 'url')
